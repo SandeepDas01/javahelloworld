bd38f94cd26e0f46fff4056091b16d74d84c4fe2
sandeepdas <sandeepdas8080@gmail.com>
2021-07-05 14:30:59 +0530

    my first commit

---
+++ helloprint.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+public class helloprint 
+{
+	
+	
+	
+
+
+		   public static void main(String []args)
+ {
+		      System.out.println("Hello "); // prints Hello World
+		   }
+	
+
+
+}
